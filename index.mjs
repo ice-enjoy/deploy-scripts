7731e116496f564ce07912d22ff9483c13e5649b
ice-enjoy <ice@enjoy.tech>
2022-09-19 23:43:52 +0800

    init

---
+++ index.mjs
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+import { AppConfig } from "./lib/config.mjs";
+import DeployScript from "./lib/script.mjs";
+
+console.log("\n");
+console.log("AppConfig: ");
+console.log(AppConfig);
+console.log("\n");
+
+let app = null;
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "app",
+      message: "Choose App to deploy:",
+      choices: AppConfig.map((app) => app.name),
+    },
+    {
+      type: "list",
+      name: "env",
+      message: "deploy To",
+      choices: ["Main", "Test"],
+    },
+  ])
+  .then((answers) => {
+    app = AppConfig.filter((app) => app.name === answers.app)[0];
+    app.env = answers.env;
+    return inquirer.prompt([
+      {
+        type: "confirm",
+        name: "deployConfirm",
+        message: `Deploy ${app.name} to env ${answers.env} at port ${app.port} for sure?`,
+      },
+    ]);
+  })
+  .then((confirm) => {
+    if (confirm.deployConfirm) {
+      DeployScript(app);
+    }
+  });
